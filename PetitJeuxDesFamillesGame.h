bd10c6a8ed9a7f32550a3b9e17e8f9ffebfb9fb8
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-07-19 15:41:54 +0200

    Create PetitJeuxDesFamillesGame.h

---
+++ PetitJeuxDesFamillesGame.h
@@ -0,0 +1,45 @@
+//
+// Created by Mathias Garnier on 19/07/2016.
+//
+
+#ifndef PETITJEUXDESFAMILLES_GAME_H
+#define PETITJEUXDESFAMILLES_GAME_H
+
+#include <string>
+
+unsigned int energieForAttack(std::string attackName);
+bool isPlayerAttack();
+void attack(std::string attackName); //attackName -> PetitJeuxDesFamilles::Attaques.
+void defense(std::string defenseName); //defenseName -> PetitJeuxDesFamilles::Defense
+
+class PetitJeuxDesFamilles {
+
+    public:
+        static constexpr const char*  GAME_NAME = "PetitJeuxDesFamilles";
+        constexpr static unsigned int MAX_ENERGIE = 80;
+        constexpr static unsigned int MAX_LIFE = 100;
+        constexpr static unsigned int MIN_LIFE = 0;
+
+    class Attaques {
+
+        public:
+            static constexpr const char* ATTACK_NAME_qdf = "Queue de fer - 35 pv de degats, utilisation de 40 energies.";
+            static constexpr const char* ATTACK_NAME_l = "Lechouille - 30 pv de degats, utilisation de 20 energies.";
+            static constexpr const char* ATTACK_NAME_lcal = "La chatte a l'air - 25 pv de degats, utilisation de 10 energies.";
+            static constexpr const char* ATTACK_NAME_tltlcalv = "Toucher la, Toucher la chatte a la voisine - 30 pv de degats, utilisation de 20 energies.";
+
+    };
+
+    class Defense {
+
+    public:
+        static constexpr const char* DEFENSE_NAME_c = "Carabistouille - 10 pv ajoute, utilisation de 30 energies.";
+        static constexpr const char* DEFENSE_NAME_is = "Infirmiere sexy - 15 pv ajoute, utilisation de 35 energies.";
+        static constexpr const char* DEFENSE_NAME_hc = "Hallucination collective - 20 pv ajoute, utilisation de 40 energies.";
+        static constexpr const char* DEFENSE_NAME_fhritp = "Fuck her right in the pussy - 15 pv ajoute, utilisation de 35 energies.";
+
+    };
+};
+
+
+#endif //PETITJEUXDESFAMILLES_GAME_H
