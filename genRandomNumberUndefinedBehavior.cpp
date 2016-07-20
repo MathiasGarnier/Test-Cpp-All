76c2b84e20909416cd024adf75761b5e5573c531
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-07-20 16:08:09 +0200

    Create genRandomNumberUndefinedBehavior.cpp

---
+++ genRandomNumberUndefinedBehavior.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <time.h>
+
+int main () {
+
+    int s[randomize()];
+    std::cout << s[0];
+    //Rebuild and rerun the program few times.
+    
+}
+
+unsigned int randomize() {
+
+    srand(time(NULL));
+    unsigned int randomNumber = (rand() % 9);
+
+    return randomNumber;
+}
