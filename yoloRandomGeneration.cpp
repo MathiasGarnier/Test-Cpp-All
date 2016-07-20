cb59979480b04fd35c515ff49d42d1cfed6484c2
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-07-20 16:48:06 +0200

    Create yoloRandomGeneration.cpp

---
+++ yoloRandomGeneration.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <time.h>
+#include <array>
+
+
+int yoloRandomGeneration(int n, int m);       //n -> colonnes, m -> lignes.
+
+int main () {
+    std::cout << yoloRandomGeneration(9, 9);
+
+}
+
+int yoloRandomGeneration(int n, int m) {
+
+    srand(time(NULL));
+
+   std::array<int, 30> N { {(rand() %10), (rand() %10), (rand() %10)} };
+}
