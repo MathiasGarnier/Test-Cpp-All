f8b008cccd5878e1a1739f7f09a5240715eb83fa
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-07-20 14:14:15 +0200

    Create FakeFileSystemMain.cpp

---
+++ FakeFileSystemMain.cpp
@@ -0,0 +1,14 @@
+#include "FakeFileSystem.h"
+
+/*
+ * Training : Fake File System.
+ */
+
+int main () {
+
+    FakeFileSystem f;
+    createFile(f); //huhu
+    //createFile(f.getName("hello"))        ;;TO BE CONTINUED
+
+    return 0;
+}
