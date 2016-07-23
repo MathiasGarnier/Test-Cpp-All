4337275d105696c98410729ced562a6c1e328db3
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-07-23 15:36:44 +0200

    Create #defineYolo.cpp

---
+++ #defineYolo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define yap for( ; ; )
+#define sayHello std::cout << "Hello" << std::endl;
+#define ceciEstUnCommentaire //
+#define commentaire "Obligé de faire comme ça huhu x)"
+
+int main() {
+
+    yap
+        sayHello ceciEstUnCommentaire commentaire;
+
+    //return 0; Unused
+}
