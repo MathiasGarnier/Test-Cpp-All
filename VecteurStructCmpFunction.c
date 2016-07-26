a27d4fab15ba56098de9104a1c99be9ef9873126
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-07-26 20:35:33 +0200

    Update VecteurStructCmpFunction.c

--- VecteurStructCmpFunction.c
+++ VecteurStructCmpFunction.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct      Vecteur Vecteur;
+typedef struct      Vector Vector;
 
-struct Vecteur {
+struct Vector {
 
-    double          longueur;           /* En cm.                                                           */
-    char            sens[2];            /* Exemple : sens[0] = 'A'; sens[1] = 'B'. Donc : vecteur AB.       */
-    char            direction[2];       /* Nom de la droit exempe : direction AB #ConfusMaisBonHuhu         */
+    double          length;           /* In centimeter.                                                   */
+    char            way[2];            /* Example : sens[0] = 'A'; sens[1] = 'B'. So : vector(AB)  .       */
+    char            direction[2];       /* Name of the line example : direction AB #Huhu                    */
 };
 
-void cmpVecteur(Vecteur v1, Vecteur v2);
+void vectorCompare(Vector v1, Vector v2);
 
 int main() {
 
-    Vecteur                   v;
+    Vector                    v;
 
-    v.longueur          =     8;
+    v.length            =     8;
     v.direction[0]      =   'A';
     v.direction[1]      =   'B';
-    v.sens[0]           =   'A';
-    v.sens[1]           =   'B';
+    v.way[0]            =   'A';
+    v.way[1]            =   'B';
 
-    Vecteur                  v1;
+    Vector                   v1;
 
-    v1.longueur         =     8;
+    v1.length           =     8;
     v1.direction[0]     =   'A';
     v1.direction[1]     =   'B';
-    v1.sens[0]          =   'A';
-    v1.sens[1]          =   'B';
+    v1.way[0]           =   'A';
+    v1.way[1]           =   'B';
 
-    cmpVecteur(v, v1);
+    vectorCompare(v, v1);
     //YOLO PATE A L'EAU
     return 0;
 }
 
-void cmpVecteur(Vecteur v1, Vecteur v2) {
+void vectorCompare(Vector v1, Vector v2) {
 
-    if ( (v1.longueur == v2.longueur) && ( (v1.direction[0] == v2.direction[0]) && (v1.direction[1] == v2.direction[1]) ) && ( (v1.sens[0] == v2.sens[0]) && ( v1.sens[1] == v2.sens[1])))
+    if ( (v1.length == v2.length) && ( (v1.direction[0] == v2.direction[0]) && (v1.direction[1] == v2.direction[1]) ) && ( (v1.way[0] == v2.way[0]) && ( v1.way[1] == v2.way[1])))
         printf("Equals");
     /*
-     * @TODO Ajouter pour si le quadrilatere ABCD est un parallelogramme (eventuellement aplati)
+     * @TODO Ajouter pour si le quadrilatere ABCD est un parallelogramme (eventuellement aplati).
      * @TODO Ajouter pour si la translation qui transforme A en B est transforme aussi C en D.
+     * Ni vu ni connu les deux dernières lignes de français héhé x).
      */
     else
         printf("Different");
