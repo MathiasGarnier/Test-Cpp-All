7d3584f71bab8b7c46ea03252f9a0c9d610e551e
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-07-28 13:47:31 +0200

    Create HelloWorldWinAPI.c

---
+++ HelloWorldWinAPI.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WinMain (HINSTANCE intance, HINSTANCE oldInstance, LPSTR commandLine, int screenMode) {
+
+    MessageBox(NULL, TEXT("Hello World &é'àèéç_-'à-"), TEXT("Title"), 0);
+    return 0;
+}
