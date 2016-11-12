04ca76cad3abfea9f3518d5d71fd9654622fa6dc
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-11-12 10:50:41 +0100

    Create #defineYolo+.cpp

---
+++ #defineYolo+.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define startProgram int main()
+#define yap for( ; ; )
+#define sayAnything(content) std::cout << content << std::endl;
+#define comment //
+#define commentaire "Obligé de faire comme ça huhu x)";
+#define returnNoError return 0;
+
+startProgram {
+
+    yap
+        sayAnything("Salut toi") comment commentaire
+
+    returnNoError
+}
